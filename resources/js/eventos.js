abd9319f507465eee412a8cafab9940ae627fbc0
Oscar Illescas <oscar.illescas@gmail.com>
2015-03-18 16:45:01 +0100

    optimizando js....

--- resources/js/eventos.js
+++ resources/js/eventos.js
@@ -1,23 +1,40 @@
-function init()
-{
-    $('#calendar').fullCalendar({
-            lang: 'es',
-            events: function(start, end, timezone, callback) {
-                console.log(arguments);
-                var events = [];
-                $(".evento").each(function(){
-                     events.push({
-                        id: $(this).attr("id"),
-                        title: $(this).find('[itemprop="name"]').text(),
-                        start: $(this).find('[itemprop="startDate"]').attr("content"), // will be parsed
-                        allDay: false,
-                        editable: false
+(function($){
+    function init()
+    {
+        $('#calendar').fullCalendar({
+                lang: 'es',
+                events: function(start, end, timezone, callback) {
+                    var events = [];
+                    $(".evento").each(function(){
+                         events.push({
+                            id: $(this).attr("id"),
+                            title: $(this).find('[itemprop="name"]').text(),
+                            start: $(this).find('[itemprop="startDate"]').attr("content"), // will be parsed
+                            description : $(this).find('[itemprop="description"]').text(),
+                            allDay: false,
+                            editable: false
+                        });
                     });
-                });
-                callback(events);
+                    callback(events);
+                },
+                 eventClick: function(calEvent, jsEvent, view) {
+                    console.log(calEvent);
+                    //console.log(jsEvent);
+                    //console.log(view);
+                    $(".showpop").removeClass("showpop")
+                      .popover('hide');
+                     $(this).popover({
+                        title : calEvent.title,
+                        content: calEvent.description,
+                        placement: 'bottom',
+                        container: 'body'
+                    })
+                     .popover('show')
+                     .addClass('showpop');
             }
-    });
-}
+        });
+    }
 
 
-$(document).ready(init);
+    $(document).ready(init);
+})(jQuery);
