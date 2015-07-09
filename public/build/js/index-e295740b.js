ecc3f06006baac8e6ae32161b22753cf7ab94b17
Oscar Illescas <oscar.illescas@gmail.com>
2015-07-09 20:03:55 +0200

    estaticos para heroku

---
+++ public/build/js/index-e295740b.js
@@ -0,0 +1,113 @@
+(function($){
+    console.log("cargando modulo maps 3.");
+    var maps = {};
+    maps.init = function(){
+
+        var $el = $("#mapDiv");
+        if($el.length ==1){
+
+
+
+            var multi = $el.data('multi');
+            var queryDatos = $el.data('origen');
+            var $datos = $(queryDatos);
+            var centro = new Microsoft.Maps.Location(40.417062, -3.703552);
+            var zoom = 12;
+
+            if(!multi){
+                centro = new Microsoft.Maps.Location($datos.find('[itemprop="latitude"]').attr('content'), $datos.find('[itemprop="longitude"]').attr('content'));
+                zoom = 14;
+            }
+
+            var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"),
+            {credentials:"AoBhurcO68zEsZnqzAYV2vZJ-mC_7CJ2ed1OndVK29akMiWNMf0EdbddQm00mqDI",
+             center: centro,
+             enableSearchLogo: false,
+             showMapTypeSelector: false,
+             zoom: zoom
+            });
+
+
+            $datos.each(function(){
+                var $centro = $(this);
+                // Define the pushpin location
+                var lat = $centro.find('[itemprop="latitude"]').attr('content');
+                var lon = $centro.find('[itemprop="longitude"]').attr('content');
+                var loc = new Microsoft.Maps.Location(lat,lon);
+
+                // Add a pin to the map
+                var pin = new Microsoft.Maps.Pushpin(loc,{draggable: false});
+                var  pinInfobox = new Microsoft.Maps.Infobox(pin.getLocation(),
+                {
+                 description: $centro.html(),
+                 visible: false,
+                 width :300, height :200,
+                 offset: new Microsoft.Maps.Point(0,15)});
+
+                Microsoft.Maps.Events.addHandler(pin, 'click', displayInfobox);
+
+                // Hide the infobox when the map is moved.
+                Microsoft.Maps.Events.addHandler(map, 'viewchange', hideInfobox);
+
+                 function displayInfobox(e)
+                 {
+                    pinInfobox.setOptions({ visible:true });
+                    map.setView({center: loc, zoom: 15});
+                 }
+
+                 function hideInfobox(e)
+                 {
+                    //pinInfobox.setOptions({ visible: false });
+                 }
+
+                map.entities.push(pin);
+                map.entities.push(pinInfobox);
+            });
+
+
+        }
+    };
+    $(document).ready(maps.init);
+})(jQuery);
+(function($){
+    function init()
+    {
+        $('#calendar').fullCalendar({
+                lang: 'es',
+                events: function(start, end, timezone, callback) {
+                    var events = [];
+                    $(".evento").each(function(){
+                         events.push({
+                            id: $(this).attr("id"),
+                            title: $(this).find('[itemprop="name"]').text(),
+                            start: $(this).find('[itemprop="startDate"]').attr("content"), // will be parsed
+                            description : $(this).find('[itemprop="description"]').text(),
+                            allDay: false,
+                            editable: false
+                        });
+                    });
+                    callback(events);
+                },
+                 eventClick: function(calEvent, jsEvent, view) {
+                    console.log(calEvent);
+                    //console.log(jsEvent);
+                    //console.log(view);
+                    $(".showpop").removeClass("showpop")
+                      .popover('hide');
+                     $(this).popover({
+                        title : calEvent.title,
+                        content: calEvent.description,
+                        placement: 'bottom',
+                        container: 'body'
+                    })
+                     .popover('show')
+                     .addClass('showpop');
+
+            }
+        });
+    }
+
+
+    $(document).ready(init);
+})(jQuery);
+//# sourceMappingURL=index.js.map
